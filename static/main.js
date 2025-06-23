b65254cba8bde4ba64c883456977c4310747702b
cataiovita <cataliniovita@yahoo.com>
2025-06-23 18:15:37 +0300

    Modify content of html file

---
+++ static/main.js
@@ -0,0 +1,7 @@
+// Source: https://openmrs-bucket.s3.eu-north-1.amazonaws.com/main.js
+
+// S3 bucket link for reference:
+// https://openmrs-bucket.s3.eu-north-1.amazonaws.com/
+const s3BucketUrl = "https://openmrs-bucket.s3.eu-north-1.amazonaws.com/";
+
+document.addEventListener('DOMContentLoaded', () => { console.log('App loaded successfully.'); }); 
